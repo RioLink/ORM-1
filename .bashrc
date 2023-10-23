3c4f2b8099693cf0eebaa0353d79539867f8f117
Alex <oleksandr.kudelya96@gmail.com>
2023-10-23 16:43:10 +0300

    orm2

---
+++ .bashrc
@@ -0,0 +1,2 @@
+export DATABASE_URL='mysql://enpsofi1l67btcps4yss:pscale_pw_l6XCiQZrbf2xhJvf9qvNe4uXmBgwNf8GPYYjAqUSIcp@aws.connect.psdb.cloud/todo?sslmode=no-verify'
+PORT=8000
--- app.js
+++ app.js
@@ -1,7 +1,56 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
+const cors = require("cors");
+const { Task } = require("./models");
+const bodyParser = require("body-parser");
 
+app.use(cors());
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(process.env.PORT);
+app.get("/api/v1/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.findAll();
+    res.json({ tasks });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Помилка сервера" });
+  }
+});
+
+app.post("/api/v1/tasks", async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const task = await Task.create({ name });
+    res.status(201).json({ task });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Помилка сервера" });
+  }
+});
+
+app.delete("/api/v1/tasks/:id", async (req, res) => {
+  const taskId = req.params.id;
+
+  try {
+    const task = await Task.findByPk(taskId);
+
+    if (!task) {
+      return res.status(404).json({ error: "Задача не знайдена" });
+    }
+
+    await task.destroy();
+    res.json({ message: "Задачу видалено" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Помилка сервера" });
+  }
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущено на порті ${PORT}`);
+});
